1d23857675f809469c9b2ca24f73a4e4421726c3
12jerek34 <12jerek34@gmail.com>
2022-05-11 10:35:08 +0200

    Fix of bug in setup.py file of hpcw package.
    Before list of packages was incomplete, there were no subpackages there.

--- hpcw/setup.py
+++ hpcw/setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup
 
-setup(
-   name='hpcw',
-   version='0.1.0',
-   author='Jedrzej Chmiel',
-   author_email='jedrzej.chmiel.ml@gmail.com',
-   install_requires=[
-      "torch",
-      "nltk == 3.7"
-   ],
-   packages=['hpcw']
-)
+setup(name='hpcw',
+      version='0.1.0',
+      author='Jedrzej Chmiel',
+      author_email='jedrzej.chmiel.ml@gmail.com',
+      install_requires=["torch", "nltk == 3.7"],
+      packages=['hpcw', 'hpcw.datasets', 'hpcw.models'])
