b92bf35050e544881dd71c3956bdbb9967e2e490
12jerek34jeremi <65294117+12jerek34jeremi@users.noreply.github.com>
2022-05-15 19:23:27 +0200

    Update setup.py

--- hpcw/setup.py
+++ hpcw/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hpcw',
-      version='0.1.0',
+      version='0.1.2',
       author='Jedrzej Chmiel',
       author_email='jedrzej.chmiel.ml@gmail.com',
       install_requires=["torch", "nltk == 3.7"],
